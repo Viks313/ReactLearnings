7317941e168382f68f1202ad42b958eef59b5075
Viks313 <saanavikas.3@gmail.com>
2024-10-04 08:50:20 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const heading=React.createElement("h1",{id:"heading"},"Hello world from react");
+
 const parent=React.createElement("div",{id:"parent"},
     [React.createElement("div",{id:"child"},
-        [React.createElement("h2",{},"react using normal way"),React.createElement("h3",{},"react using normal way")]
+        [React.createElement("h2",{},"react using normal way"),
+        React.createElement("h3",{},"react using normal way")]
     ),React.createElement("div",{id:"child2"},
-        [React.createElement("h2",{},"react using normal way"),React.createElement("h3",{},"react using normal way")]
+        [React.createElement("h2",{},"react using normal way"),
+        React.createElement("h3",{},"react using normal way")]
     )]
 )
 const root= ReactDOM.createRoot(document.getElementById("root"));
